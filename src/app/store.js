1aead0641846c18738dedd57786df541d98f86a4
denizderK <denizderya8668@gmail.com>
2023-04-07 21:23:15 +0300

    edit

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,3 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import blogReducer from "../features/blogSlice"
-
-// const store = configureStore({
-//   reducer:{
-//     blog:blogReducer
-//   },
-//   devTools: process.env.NODE_ENV !== "production",
-// })
-// export default store
 import { configureStore } from "@reduxjs/toolkit"
 import authReducer from "../features/authSlice"
 import blogReducer from "../features/blogSlice"
